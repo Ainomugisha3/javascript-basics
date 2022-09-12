1f8323786fb20b08a81139af6f4c09fba1aab7ab
Isaac Ainomugisha <ainomugishaisaac@gmail.com>
2022-09-12 20:32:41 +0300

    basic  javascript

---
+++ Isaac.js
@@ -0,0 +1,58 @@
+let you =" are the reason";
+// string
+var we = " the next generation of programmer";
+//string
+const us = ' we are upcoming software problem solvers';
+//string
+let num1 = 5;
+//number
+let num2 = 10;
+//number
+let num3 = num2 += num1;
+//number
+let ans = num1 % num2;
+//number
+console.log(ans);
+//output
+console.log(num3 *= 2);
+//output
+var num4;
+//null
+console.log(num4);
+//undefined
+console.log(2num6);
+//output 2 and num6 is undefined  
+var ans = 'this is not right';
+//string
+var let ='do you know what you are doing?';
+//string
+const dad;
+//constant but null
+const myNum = num1>num2? num3:us;
+//number
+console.log(--num3);
+//number-1
+console.log(num3);
+//number
+console.log(num3++);
+//number+1
+console.log(num2 != num3);
+//not or equal
+console.log(num3 !=num2 && num2==num2);
+//operators
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+//if statment
+
+
+
+
+
+
+
+
+
+
